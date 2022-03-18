6704fa8b310f72eed58e71d1e245289f2148466c
Alexandr-Horton <aleksandr.horton@gmail.com>
2022-03-18 17:11:28 +0300

    Router, main page, alert

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export const Navbar = () => (
   <nav className="navbar navbar-dark bg-primary navbar-expand-lg p-3">
     <div className="navbar-brand">
-      Github Поиск
+      Github Search
     </div>
     <ul className="navbar-nav">
       <li className="nav-item">
-        <a href="/" className="nav-link">Главная</a>
+        <NavLink exact to="/" className="nav-link">Main</NavLink>
       </li>
       <li className="nav-item">
-        <a href="/about" className="nav-link">Информация</a>
+        <NavLink to="/about" className="nav-link">Information</NavLink>
       </li>
     </ul>
   </nav>
